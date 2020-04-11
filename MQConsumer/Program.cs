460b2202f40b203d3fbdc88c31512049e15ef228
wei <120365850@qq.com>
2020-04-11 19:08:53 +0800

    first commit

---
+++ MQConsumer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//队列的消费者
+namespace MQConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("队列的消费者 starting!");
+            Start();
+        }
+
+        static void Start()
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "120.79.240.230"
+            };
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+            
+            //事件基本消费者
+            EventingBasicConsumer ebConsumer=new EventingBasicConsumer(channel);
+            
+            //接收到消息事件
+            ebConsumer.Received += (ch,ea) =>
+            {
+
+                var message = Encoding.UTF8.GetString(ea.Body);
+                Console.WriteLine("收到消息了："+message);
+                //确认该消息已被消费
+                channel.BasicAck(ea.DeliveryTag,false);
+            };
+            
+            //启动消费者 设置为手动应答消息
+            channel.BasicConsume("hello", false, ebConsumer);
+            
+            Console.Write("消费者已经开启");
+            Console.ReadKey();
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ MQFactory/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using RabbitMQ.Client;
+using  System.Text;
+
+//MQ生产者
+namespace MQFactory
+{
+    class Program
+    {
+        private const string queName = "hello";
+        private const string exChangeName = "exHello";
+        private const string routeKey = "hello";
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MQ生产者 starting!");
+            StartFactory();
+        }
+
+        static void StartFactory()
+        {
+            //创建工厂
+            ConnectionFactory factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "120.79.240.230"
+            };
+            
+            //创建链接
+            var connection = factory.CreateConnection();
+            
+            //创建通道
+            var channel= connection.CreateModel();
+            
+            //定义交换机
+            channel.ExchangeDeclare(exChangeName,ExchangeType.Direct,false,false,null);
+            
+            //声明队列
+            //生产者将消息投递到Queue中，实际上这在RabbitMQ中这种事情永远都不会发生
+            channel.QueueDeclare(queName,false,false,false,null);
+            
+            //将队列绑定到交换机
+            channel.QueueBind(queName,exChangeName, routeKey, null);
+            
+            Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入exit退出！");
+            
+            string input="";
+            
+            do
+            {
+                input = Console.ReadLine();
+                
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                
+                //发布消息
+                channel.BasicPublish(exChangeName, routeKey, null, sendBytes);
+                
+            } while (input?.Trim().ToLower() != "exit");
+
+            channel.Close();
+            
+            connection.Close();
+        }
+    }
+}
